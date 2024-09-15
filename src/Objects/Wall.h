a4b8dca94dc2c1ded945ef61fb6ae197ee38b407
Elias Aggergaard Larsen <s235125@dtu.dk>
2024-09-15 22:59:04 +0200

    Created wall

---
+++ src/Objects/Wall.h
@@ -0,0 +1,23 @@
+//
+// Created by Elias Aggergaard Larsen on 15/09/2024.
+//
+
+#ifndef WALL_H
+#define WALL_H
+#include <memory>
+
+#include "EngineBase/Collidable.h"
+#include "EngineBase/DrawAble.h"
+
+
+class Wall : public DrawAble
+{
+public:
+    Wall(float x, float y, int width, int height, int textureIndex);
+    ~Wall();
+    std::shared_ptr<Collidable> collidable_;
+};
+
+
+
+#endif //WALL_H
